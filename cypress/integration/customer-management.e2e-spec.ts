70ee0a691dfcb4711976c90329b4ee7b331b9360
Rasmus <hansen13579@gmail.com>
2020-10-24 14:46:01 +0200

    Update test with editing customer

--- cypress/integration/customer-management.e2e-spec.ts
+++ cypress/integration/customer-management.e2e-spec.ts
@@ -9,7 +9,29 @@ describe('customer-management', () => {
 
     cy.get('app-add-new-customer-dialog').should('not.exist');
 
-    cy.cget('customer-link').contains('test customer').should('exist').parent().cget('customer-more-button').click();
+    cy.cget('customer-link').contains('test customer').should('exist');
+
+    cy.cget('edit-customer-link').click();
+
+    cy.cget('customer-name-input').type('{selectall}edited customer');
+
+    cy.cget('add-new-environment-button').click();
+
+    cy.cget('environment-name-input').type('Test env');
+
+    cy.cget('add-product-button').click();
+
+    cy.cget('product-id-select').click();
+
+    cy.cget('product-id-option').contains('Dam Center').click();
+
+    cy.cget('product-version-select').click();
+
+    cy.cget('product-version-option').contains('1.0.1').click();
+
+    cy.cget('save-edited-customer-button').should('not.be.disabled').click();
+
+    cy.cget('customer-link').contains('edited customer').should('exist').parent().cget('customer-more-button').click();
 
     cy.cget('customer-more-delete-button').click();
 
