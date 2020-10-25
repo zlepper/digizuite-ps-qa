804369ced4fac6159a8377d6b4b6f4349df59094
Rasmus <hansen13579@gmail.com>
2020-10-25 21:00:59 +0100

    Cleanup

--- src/app/components/customers/test-product/test-product.component.ts
+++ src/app/components/customers/test-product/test-product.component.ts
@@ -1,16 +1,13 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CustomerDisplayService } from '../../../services/customer-display.service';
 import { ProductDisplayService } from '../../../services/product-display.service';
 import { CustomerEnvironmentDisplayService } from '../../../services/customer-environment-display.service';
-import { combineLatest, NEVER, Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Customer, CustomerEnvironment } from '../../../store/customer/customer.interfaces';
 import { Product, ProductUseCase } from '../../../store/product/product.interfaces';
-import { select, Store } from '@ngrx/store';
-import { switchMap } from 'rxjs/operators';
-import { getUseCasesForProduct } from '../../../store/product/product.selectors';
+import { Store } from '@ngrx/store';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { Version } from '../../../helpers';
 import { Test, TestResult } from '../../../store/test/test.interfaces';
 import { AppFirestoreService } from '../../../services/app-firestore.service';
 import { Dictionary } from '@ngrx/entity';
@@ -28,11 +25,8 @@ export class TestProductComponent implements OnInit {
   public environment$: Observable<CustomerEnvironment>;
   public product$: Observable<Product>;
   public useCases$: Observable<Dictionary<ProductUseCase>>;
-  public TestResult = TestResult;
 
   public tests$: Observable<Test[]>;
-
-  public savingTests = new Set<string>();
 
   constructor(
     private testDisplayService: TestDisplayService,
@@ -43,8 +37,8 @@ export class TestProductComponent implements OnInit {
     private snackBar: MatSnackBar,
     private router: Router,
     private firestore: AppFirestoreService,
-    private cd: ChangeDetectorRef,
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     this.testDisplayService.init();
@@ -74,7 +68,6 @@ export class TestProductComponent implements OnInit {
     }
 
     try {
-      this.savingTests.add(test.id);
       await this.firestore.testCollection.doc(test.id).set({
         ...test,
         result: nextState,
@@ -82,9 +75,6 @@ export class TestProductComponent implements OnInit {
     } catch (e) {
       console.error(e);
       this.snackBar.open('Something went wrong when updating test result, please check F12 for more information', '', { duration: 5000 });
-    } finally {
-      this.savingTests.delete(test.id);
-      this.cd.markForCheck();
     }
   }
 }
